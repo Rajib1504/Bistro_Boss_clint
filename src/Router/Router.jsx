25d4a48751c1aff79310991f9f1a05c66849e21f
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-12-31 23:21:51 +0530

    title and icon is change and now menu is under process

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Home/Home/Home";
+import Menu from "../Pages/Menu/Menu";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -9,6 +10,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "menu",
+        element: <Menu></Menu>,
       },
     ],
   },
