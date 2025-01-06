3ada8184516cfcd9ee1932db29a3ddfce9f92ae3
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-01-06 09:01:50 +0530

    we send the cart to the data base using the axios

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -2,15 +2,36 @@ import React from "react";
 import UseAuth from "../../hooks/useAuth/UseAuth";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const FoodCard = ({ item }) => {
   const { user } = UseAuth();
   const { image, name, price, recipe } = item;
   const navigate = useNavigate();
   const handlecart = (food) => {
-    console.log(food, user?.email);
     if (user && user.email) {
+      console.log(food, user?.email);
       // todo need to send the data to database
+      const cartItem = {
+        menuId: food._id,
+        email: user.email,
+        name,
+        image,
+        price,
+      };
+      axios.post("http://localhost:5000/carts", cartItem).then((res) => {
+        console.log(res.data);
+        if (res.data.insertedId) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${name}added to your cart`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
     } else {
       Swal.fire({
         title: "You are not Loged in ",
@@ -23,7 +44,7 @@ const FoodCard = ({ item }) => {
       }).then((result) => {
         if (result.isConfirmed) {
           // send to the login page
-          navigate("/login");
+          navigate("/login", { state: { from: location } });
         }
       });
     }
