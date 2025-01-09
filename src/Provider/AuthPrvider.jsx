7f209299b289f060a8bf3ac3a1e55761a7518a6d
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-01-09 17:16:36 +0530

    token is created with many improvments

--- src/Provider/AuthPrvider.jsx
+++ src/Provider/AuthPrvider.jsx
@@ -10,9 +10,11 @@ import {
 import { useEffect, useState } from "react";
 import { createContext } from "react";
 import auth from "./../Firebase/Firebase.config";
+import UseAxiosPublic from "../hooks/AxiosSecure/UseAxiosPublic";
 export const AuthContext = createContext(null);
 
 const AuthPrvider = ({ children }) => {
+  const axiosPublic = UseAxiosPublic(); //we will use public because this everyone will konw dont need to make it secure
   const [user, setUser] = useState(null);
   const [Loading, setLoading] = useState(true);
   const googleProvider = new GoogleAuthProvider();
@@ -47,6 +49,19 @@ const AuthPrvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       console.log("currentUser", currentUser);
+      if (currentUser) {
+        //get token and store Client
+        const userInfo = { email: currentUser.email }; // take the email from the current login user
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          // console.log(res.data.token);
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
+        });
+      } else {
+        // TODO: remove GiToken(if token stored in the clint side: Local Storage,caching,in memory )for cookie we need to remove it form the backend
+        localStorage.removeItem("access-token"); // we will remove the token
+      }
       setLoading(false);
     });
     return () => {
