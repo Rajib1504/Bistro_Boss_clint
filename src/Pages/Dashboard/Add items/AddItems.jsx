cd014ec8129f6c6b1a27c3593ced7dc8d692c8b5
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-01-11 23:45:11 +0530

    image is converted to the link which will help user to see the link and get the image

--- src/Pages/Dashboard/Add items/AddItems.jsx
+++ src/Pages/Dashboard/Add items/AddItems.jsx
@@ -1,17 +1,23 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
-
+import UseAxiosPublic from "../../../hooks/AxiosSecure/UseAxiosPublic";
 // image hosting key
 const image_hosting_key = import.meta.env.VITE_Image_Hosting_Key;
-// img hosting api
+// img hosting apiimport UseAxiosPublic from
 const hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
+
 const AddItems = () => {
   const { register, handleSubmit } = useForm();
-
-  const onSubmit = (data) => {
+  const axiosPublic = UseAxiosPublic();
+  const onSubmit = async (data) => {
     console.log(data);
     //image uploade to imgbb and then get an url
+    const imageFile = { image: data.image[0] };
+    const res = await axiosPublic.post(hosting_api, imageFile, {
+      headers: { "content-type": "multipart/form-data" },
+    });
+    console.log(res.data);
   };
 
   return (
