001a9ada3d968b0ef14296a2c44b581128ce85cf
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-01-11 20:16:33 +0530

    we have created the add item form

--- src/Pages/Dashboard/Add items/AddItems.jsx
+++ src/Pages/Dashboard/Add items/AddItems.jsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { FaUtensils } from "react-icons/fa";
 
 const AddItems = () => {
   const { register, handleSubmit } = useForm();
@@ -11,30 +12,74 @@ const AddItems = () => {
   return (
     <div>
       <SectionTitle
-        heading={"add an item"}
+        heading={"Add an item"}
         subHeading={"What's new?"}
       ></SectionTitle>
-      <div className="bg-pink-50 p-2">
+      <div className="bg-pink-50 p-10 mx-auto rounded-md shadow-sm min-w-md">
         <form onSubmit={handleSubmit(onSubmit)}>
           {/* register your input into the hook by invoking the "register" function */}
-          <input defaultValue="test" {...register("Name")} />
-          {/* include validation with required or other standard HTML validation rules */}
-          <select
-            {...register("category")}
-            className="select select-bordered w-full max-w-xs"
-          >
-            <option value="salad">salad</option>
-            <option value="drinks">drinks</option>
-            <option value="soup">soup</option>
-            <option value="pizza">pizza</option>
-            <option value="dessert">dessert</option>
-            <option value="popular">popular</option>
-          </select>
+          <div className="form-control gap-6">
+            <label className="text-gray-500">
+              {" "}
+              Recipe name*
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full "
+                {...register("name", { required: true })}
+              />
+            </label>
+            {/* include validation with required or other standard HTML validation rules */}
+            <div className=" flex gap-3 items-center w-full ">
+              <label className="flex-1 text-gray-500">
+                Category*
+                <select
+                  {...register("category", { required: true })}
+                  className="select select-bordered w-full"
+                  defaultValue={"category"}
+                >
+                  <option defaultValue="category">Category</option>
+                  <option value="salad">salad</option>
+                  <option value="drinks">drinks</option>
+                  <option value="soup">soup</option>
+                  <option value="pizza">pizza</option>
+                  <option value="dessert">dessert</option>
+                  <option value="popular">popular</option>
+                </select>
+              </label>
+              <label className="flex-1 text-gray-500">
+                Price*
+                <input
+                  type="number"
+                  placeholder="Price"
+                  className="input input-bordered w-full"
+                  {...register("price", { required: true, minLength: 1 })}
+                />
+              </label>
+            </div>
 
-          <input {...register("exampleRequired", { required: true })} />
-          {/* errors will return when field validation fails  */}
-          <span>This field is required</span>
-          <input type="submit" />
+            {/* recipe details  */}
+            <div>
+              <label className=" text-gray-500">
+                {" "}
+                Recipe Details*
+                <textarea
+                  {...register("recipe", { required: true })}
+                  type="number"
+                  placeholder="Recipe Details"
+                  className="textarea textarea-bordered row-span-6 textarea-xl h-36 w-full  "
+                ></textarea>
+              </label>
+            </div>
+            <input
+              {...register("image", { required: true })}
+              type="file"
+              className=" text file-input w-full max-w-xs"
+            />
+          </div>
+          <button className="btn my-4 rounded-none text-white flex bg-yellow-400">
+            Add Item <FaUtensils className="ml-4"></FaUtensils>
+          </button>
         </form>
       </div>
     </div>
